567637fcefe5c24eed4fec282f8539c4bf56a8bc
chishige1217200 <daichicchi01@gmail.com>
2020-12-10 22:55:38 +0900

    [clean] リファクタリング

--- Assets/Scripts/GameScene/Mine.cs
+++ Assets/Scripts/GameScene/Mine.cs
@@ -2,30 +2,36 @@ using System;
 using System.Threading.Tasks;
 using UnityEngine;
 
-public class Mine : MonoBehaviour {
+public class Mine : MonoBehaviour
+{
     private Renderer rend;
-    void Start () {
+    void Start()
+    {
         //rend = GameObject.Find ("Big Explosion").GetComponent<Renderer> ();
-        Gobomb (); //GoBomb関数実行
+        Gobomb(); //GoBomb関数実行
     }
 
-    void Update () {
-
-    }
-
-    async void Gobomb () {
-        Prediction (); //爆発カウントダウン実行
-        await Task.Delay (5000); //5秒待機
-        Explosion (); //爆発関数実行
-    }
-
-    void Prediction () {
-        Debug.Log ("Predict!"); //ログ出力
+    void Update()
+    {
 
     }
 
-    void Explosion () {
-        Debug.Log ("Bomb!"); //ログ出力
+    async void Gobomb()
+    {
+        Prediction(); //爆発カウントダウン実行
+        await Task.Delay(5000); //5秒待機
+        Explosion(); //爆発関数実行
+    }
+
+    void Prediction()
+    {
+        Debug.Log("Predict!"); //ログ出力
+
+    }
+
+    void Explosion()
+    {
+        Debug.Log("Bomb!"); //ログ出力
     }
 
 }
